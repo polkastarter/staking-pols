573b688f213732dbb16d0bb4ea0fa7bbfc6a3353
Sven Meyer <git_dev@svenmeyer.com>
2022-05-26 17:11:14 +1000

    test: make tokens more generic ERC20 tokens

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -5,15 +5,15 @@ import { Signers } from "./";
 
 import { PolkastarterToken } from "../typechain/PolkastarterToken";
 // import { RewardToken } from "../typechain/RewardToken";
-import { ERC20 } from "../typechain/ERC20";
+import { IERC20 } from "../typechain/IERC20";
 import { IERC20Metadata } from "../typechain/IERC20Metadata";
 
 import { PolsStake } from "../typechain/PolsStake";
 
 declare module "mocha" {
   export interface Context {
-    stakeToken: ERC20 & IERC20Metadata;
-    rewardToken: ERC20 & IERC20Metadata;
+    stakeToken: IERC20 & IERC20Metadata;
+    rewardToken: IERC20 & IERC20Metadata;
     stake: PolsStake;
 
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
